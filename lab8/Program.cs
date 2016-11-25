175589bc8fc616bdd5bb041d2229780b052f9cde
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2016-11-25 13:47:45 +0100

    lab8 completed

---
+++ lab8/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+
+namespace Lab8a
+{
+class MainClass
+    {
+        public static void Main (string[] args)
+            {
+            IEnumerable seq1 = Sequences.ArithmeticSequence(2, 3);
+            IEnumerable seq2 = Sequences.ArithmeticSequence(1, 1);
+            Console.Out.WriteLine("Etap 1:");
+            Sequences.PrintN(new int[] { 1, 2, 3, 4 }, 4);
+            Sequences.PrintN(new int[] { 1, 2, 3, 4 }, 2);
+            Sequences.PrintN(new int[] { 1, 2, 3, 4 }, 10);
+            Sequences.PrintN(seq1, 10);
+            Sequences.PrintN(seq1, 10);
+            Sequences.PrintN(seq2, 20);
+            Sequences.PrintN(Sequences.SubtractX(seq1, 8), 10);
+            Sequences.PrintN(Sequences.SubtractX(new int[] { -2, 2, -2, 2 }, 8), 10);
+
+            Console.Out.WriteLine("\nEtap 2:");
+            Sequences.PrintN(Sequences.RepeatNTimes(new int[] { 1, 2, 3 }, 3), 20);
+            Sequences.PrintN(Sequences.RepeatNTimes(seq1, 3), 20);
+            Sequences.PrintN(Sequences.RepeatNTimes(seq2, 3), 20);
+            Console.Out.WriteLine("LastOrDefault:");
+            Console.Out.WriteLine(Sequences.LastOrDefault(new int[] { 1, 2, 3 }, 0));
+            Console.Out.WriteLine(Sequences.LastOrDefault(new int[] { }, 0));
+            Console.Out.WriteLine("LimitSeq:");
+            Sequences.PrintN(Sequences.LimitSeq(seq1, 10), 20);
+            Sequences.LastOrDefault(Sequences.LimitSeq(seq1, 10), -1024);
+
+            Console.Out.WriteLine("\nEtap 3:");
+            Console.Out.WriteLine(Sequences.InnerProduct(new int[] { 1, 2, 3 }, new int[] { 0, 5, 1 }));
+            Console.Out.WriteLine(Sequences.InnerProduct(new int[] { 1, 2, 3, 1, 1, 1, 1 }, new int[] { 0, 5, 1 }));
+            Console.Out.WriteLine(Sequences.InnerProduct(new int[] { 1, 2, 3 }, seq1));
+            Console.Out.WriteLine(Sequences.InnerProduct(new int[] { }, seq1));
+            Console.Out.WriteLine(Sequences.InnerProduct(seq1, new int[] { }));
+
+            Console.Out.WriteLine("NotDividingNeighboursSubsequence");
+            Sequences.PrintN(Sequences.NotDividingNeighboursSubsequence(seq1), 10);
+            Sequences.PrintN(Sequences.NotDividingNeighboursSubsequence(new int[] { 2, 4, 2, 4, 2, 4, 2, 4, 5, 13, 26, 2 }), 10);
+            Sequences.PrintN(Sequences.NotDividingNeighboursSubsequence(new int[] { }), 10);
+            Sequences.PrintN(Sequences.NotDividingNeighboursSubsequence(new int[] { 2 }), 10);
+
+            Console.Out.WriteLine("\nEtap 4:");
+            Sequences.PrintN(Sequences.Interleave(new int[] { 1, 2, 3, 4 }, new int[] { -1, -2, -3, -4 }), 20);
+            Sequences.PrintN(Sequences.Interleave(new int[] { }, new int[] { }), 20);
+            Sequences.PrintN(Sequences.Interleave(seq1, seq2), 20);
+            Sequences.PrintN(Sequences.Interleave(seq1, new int[] { }), 20);
+            Sequences.PrintN(Sequences.Interleave(new int[] { }, seq1), 20);
+            Sequences.PrintN(Sequences.Interleave(new int[] { -1, -2, -3 }, seq1), 20);
+            Sequences.PrintN(Sequences.Interleave(seq1, new int[] { -1, -2, -3, -4 }), 20);
+
+            Console.Out.WriteLine("\nEtap 5:");
+            Console.Out.WriteLine(Sequences.IsRecurrenceEquation(new int[] { -1, 1, -1, 1, -1, 1, -1, 1 }, new int[] { -1 }));
+            Console.Out.WriteLine(Sequences.IsRecurrenceEquation(new int[] { -1, 1, -1, 1, -2, 1, -1, 1 }, new int[] { -1 }));
+            Console.Out.WriteLine(Sequences.IsRecurrenceEquation(new int[] { -1, 2, -1, 1, -1, 1, -1, 1 }, new int[] { -1 }));
+            Console.Out.WriteLine(Sequences.IsRecurrenceEquation(new int[] { 1, 3, 5, 2, 1, -2, 7, 0, 13, -18, 27 }, new int[] { 0, 1, -2, 2 }));
+            Console.Out.WriteLine(Sequences.IsRecurrenceEquation(new int[] { }, new int[] { 0, 1, -2, 2 }));
+            Console.Out.WriteLine(Sequences.IsRecurrenceEquation(new int[] { 1, 3, 5 }, new int[] { 0, 1, -2, 2 }));
+            Console.Out.WriteLine(Sequences.IsRecurrenceEquation(new int[] { 0, 1, 2, 3 }, new int[] { 0, 1, -2, 2 }));
+            Console.Out.WriteLine(Sequences.IsRecurrenceEquation(new int[] { 1, 3, 5, 0, 1, -2, 7, 0, 13, -18, 27 }, new int[] { 0, 1, -2, 2 }));
+            Console.Out.WriteLine(Sequences.IsRecurrenceEquation(new int[] { 1, 3, 5, 2, 1, -2, 7, 0, 13, 18, 27 }, new int[] { 0, 1, -2, 2 }));
+            Console.Out.WriteLine(Sequences.IsRecurrenceEquation(new int[] { 1, 3, 5, 0, 1, -2, 7, 0, 13, -18, 27 }, new int[] { 0, 1, -2, 2 }));
+            Console.Out.WriteLine(Sequences.IsRecurrenceEquation(new int[] { 1, 3, 5, 2, 1, -2, 7, 0, 13, -18, 27 }, new int[] { 1, 1, -2, 2 }));
+            Console.Out.WriteLine(Sequences.IsRecurrenceEquation(new int[] { 1, 3, 5, 2, 1, -2, 7, 0, 13, 18, 27 }, new int[] { 0, 0, 0, 0 }));
+            Console.Out.WriteLine(Sequences.IsRecurrenceEquation(new int[] { 1, 3, 5, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, new int[] { 0, 0, 0, 0 }));
+            Console.Out.WriteLine(Sequences.IsRecurrenceEquation(new int[] { 1, 3, 5, 2, 1, 3, 5, 2, 1, 3, 5, 2, 1, 3, 5, 2, 1, 3, 5, 2, 1, 3, 5, 2, 1, 3, 5, 2 }, new int[] { 0, 0, 0, 1 }));
+            Console.Out.WriteLine(Sequences.IsRecurrenceEquation(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 }, new int[] { 0, 0, 0, 0 }));
+        }
+    }
+}
+
---
+++ lab8/Sequences.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections;
+
+namespace Lab8a
+{
+    class Sequences
+    {
+        static public void PrintN(IEnumerable e, int n)
+        {
+            int i = 0;
+            foreach(var element in e)
+            {
+                i += 1;
+                if (i > n)
+                    break;
+                Console.Write("{0} ", element);
+            }
+            Console.WriteLine();
+        }
+
+        static public IEnumerable ArithmeticSequence(int initial, int difference)
+        {
+            return new ArithmeticSeq(initial, difference);
+        }
+
+        private class ArithmeticSeq : IEnumerable
+        {
+            private int initial;
+            private int difference;
+
+            public ArithmeticSeq(int _initial, int _difference)
+            {
+                initial = _initial;
+                difference = _difference;
+            }
+
+            public IEnumerator GetEnumerator()
+            {
+                int current = initial;
+                while (true)
+                {
+                    yield return current;
+                    current += difference;
+                }
+            }
+        }
+
+        static public IEnumerable SubtractX(IEnumerable e, int difference)
+        {
+            return new SubtractSeq(e, difference);
+        }
+
+        private class SubtractSeq : IEnumerable
+        {
+            private IEnumerable sequence;
+            private int difference;
+
+            public SubtractSeq(IEnumerable _sequence, int _difference)
+            {
+                sequence = _sequence;
+                difference = _difference;
+            }
+
+            public IEnumerator GetEnumerator()
+            {
+                IEnumerator enumerator = sequence.GetEnumerator();
+                while (enumerator.MoveNext())
+                    yield return (int)enumerator.Current - difference;
+            }
+        }
+
+
+        static public IEnumerable RepeatNTimes(IEnumerable e, int n)
+        {
+            return new RepeatNSeq(e, n);
+        }
+
+        private class RepeatNSeq : IEnumerable
+        {
+            private IEnumerable sequence;
+            private int n;
+
+            public RepeatNSeq(IEnumerable _seq, int _n)
+            {
+                sequence = _seq;
+                n = _n;
+            }
+
+            public IEnumerator GetEnumerator()
+            {
+                for (int i=0; i < n; i++)
+                {
+                    IEnumerator enumerator = sequence.GetEnumerator();
+                    while (enumerator.MoveNext())
+                        yield return enumerator.Current;
+                }
+                
+            }
+        }
+
+        static public IEnumerable LimitSeq(IEnumerable e, int n)
+        {
+            return new LimitSeqClass(e, n);
+        }
+
+        private class LimitSeqClass : IEnumerable
+        {
+            private IEnumerable sequence;
+            private int n;
+
+            public LimitSeqClass(IEnumerable _seq, int _n)
+            {
+                sequence = _seq;
+                n = _n;
+            }
+
+            public IEnumerator GetEnumerator()
+            {
+                IEnumerator enumerator = sequence.GetEnumerator();
+                int i = 0;
+                while (enumerator.MoveNext())
+                {
+                    i += 1;
+                    if (i > n)
+                        yield break;
+                    yield return enumerator.Current;
+                }
+            }
+        }
+
+        static public int LastOrDefault(IEnumerable e, int defaultVal)
+        {
+            int previous = defaultVal;
+            IEnumerator enumerator = e.GetEnumerator();
+            while (enumerator.MoveNext())
+                previous = (int)enumerator.Current;
+            return previous;
+        }
+        static public int InnerProduct(IEnumerable e1, IEnumerable e2)
+        {
+            IEnumerator enumerator1 = e1.GetEnumerator(),
+                enumerator2 = e2.GetEnumerator();
+
+            int product = 0;
+            while (enumerator1.MoveNext() && enumerator2.MoveNext())
+                product += (int)enumerator1.Current * (int)enumerator2.Current;
+
+            return product;
+        }
+
+        static public IEnumerable NotDividingNeighboursSubsequence(IEnumerable e)
+        {
+            return new NotDividingNeighboursSubsequenceClass(e);
+        }
+
+        private class NotDividingNeighboursSubsequenceClass : IEnumerable
+        {
+            private IEnumerable sequence;
+            public NotDividingNeighboursSubsequenceClass(IEnumerable _sequence)
+            {
+                sequence = _sequence;
+            }
+
+            public IEnumerator GetEnumerator()
+            {
+                int previous = -1;
+                IEnumerator enumerator = sequence.GetEnumerator();
+
+                while (enumerator.MoveNext())
+                {
+                    if (previous < 0)
+                    {
+                        previous = (int)enumerator.Current;
+                        yield return enumerator.Current;
+                        continue;
+                    }
+
+                    int current = (int)enumerator.Current;
+                    if (current % previous == 0 || previous % current == 0)
+                        continue;
+                    previous = current;
+                    yield return current;
+                }
+            }
+        }
+
+        static public IEnumerable Interleave(IEnumerable seq1, IEnumerable seq2)
+        {
+            return new InterleaveSeq(seq1, seq2);
+        }
+
+        private class InterleaveSeq : IEnumerable
+        {
+            private IEnumerable seq1;
+            private IEnumerable seq2;
+
+            public InterleaveSeq(IEnumerable _seq1, IEnumerable _seq2)
+            {
+                seq1 = _seq1;
+                seq2 = _seq2;
+            }
+
+            public IEnumerator GetEnumerator()
+            {
+                IEnumerator e1 = seq1.GetEnumerator(),
+                    e2 = seq2.GetEnumerator();
+                bool e1NotFinished;
+
+                while ((e1NotFinished = e1.MoveNext()) && e2.MoveNext())
+                {
+                    yield return e1.Current;
+                    yield return e2.Current;
+                }
+
+                if (e1NotFinished)
+                    yield return e1.Current;
+
+                while (e1.MoveNext())
+                    yield return e1.Current;
+
+                while (e2.MoveNext())
+                    yield return e2.Current;
+            }
+        }
+
+        static public bool IsRecurrenceEquation(IEnumerable sequence, int[] coefficients)
+        {
+            int n = coefficients.Length;
+            int[] lastValues = new int[n];
+            int i = 0;
+            int sum = 0;
+
+            foreach (int value in sequence)
+            {
+                if (i >= n)
+                {
+                    // Sprawdzenie sumy
+                    sum = 0;
+                    for (int j = 0; j < n; j++)
+                        sum += coefficients[j] * lastValues[j];
+                    if (sum != value)
+                        return false;
+                }
+
+                // Dodaj elementy
+                for (int j = n-1; j > 0; j--)
+                    lastValues[j] = lastValues[j - 1];
+                lastValues[0] = value;
+                i++;
+            }
+
+            return true;
+        }
+
+    }
+}
